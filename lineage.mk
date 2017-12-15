dcef7b9fe6ac89d2294c23b2bc6a4993c897bf81
Michael Gernoth <michael@gernoth.net>
2017-12-15 09:54:23 +0100

    maserati: depend on mini_phone instead of full_phone
    frees up some space on /system
    
    Change-Id: I6d0c1559df92398b608186e870e0b7fee31a2ff5

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/motorola/maserati/full_maserati.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 #
 # Setup device specific product configuration.
